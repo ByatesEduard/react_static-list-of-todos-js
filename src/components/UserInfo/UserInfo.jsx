ef81d71acfedc14a39ac30aa1b216e8a082bd250
EduardByatec <edikbatec1@gmail.com>
2025-07-03 14:15:03 +0300

    add

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const UserInfo = ({ user }) => {
+export const UserInfo = ({ user }) => {
   if (!user) return null; // на випадок, якщо користувача немає
 
   return (
